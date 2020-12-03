1a500ae8e4cecf544541d705e40812e50348de16
18130159 <59139041+18130159@users.noreply.github.com>
2020-12-03 18:24:03 +0500

    добавление нового языка прогр.

---
+++ newyaz.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		
+		<title>Хабибуллина Д.</title> 
+	</head>
+	<body style="color: #636bc7">
+		<h4>Хабибуллина Д.</h4>
+		<h1>ЛР №2</h1>
+		<h2>Вариант 11</h2>
+<h2>Добавить новый язык программирования</h2>
+<form action="newyazaction.php" metod="get">
+Название: <input name="name" size="30" type="varchar">
+<br>Тип: <input name="type" size="30" type="varchar">
+<br>Год разработки: <input name="year" size="30" type="varchar">
+<br>Тип выполнения: <input name="vipl" size="30" type="varchar">
+<br>Автор: <input name="author" size="30" cols="varchar">
+</textarea>
+<p><input name="add" type="submit" value="Добавить">
+</form>
+<p>  
+<p><a href="lr4.php" class="link">назад</a></p>	
+</div>
+	</body>
+</html>
---
+++ newyazaction.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		
+		<title>Хабибуллина Д.</title> 
+	</head>
+	<body style="color: #636bc7">
+		<h4>Хабибуллина Д.</h4>
+		<h1>ЛР №2</h1>
+		<h2>Вариант 11</h2>
+<h2>Добавить новый язык программирования</h2>
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "progr" ); 
+mysqli_query($connect, "SET NAMES utf8");
+$sql_add = "INSERT INTO progrlanguages SET name='" . $_GET['name']."', type='".$_GET['type']."', year='".$_GET['year']."', vipl='".$_GET['vipl']."', author='".$_GET['author']. "'";
+mysqli_query($connect, $sql_add); 
+if (mysqli_affected_rows($connect)>0)
+{ print "<p>Спасибо, вы добавили язык программирования.";
+print "<p><a href=\"laba4.php\"> Вернуться к списку языков</a>"; }
+else { print "Ошибка сохранения. <a href=\"laba4.php\"> Вернуться к списку языков</a>"; }
+?>
+</div>
+	</body>
+</html>

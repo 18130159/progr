7ff9f8c22fb1ce1fcdedfc00d0116bef72cc3bc1
18130159 <59139041+18130159@users.noreply.github.com>
2020-12-03 18:20:31 +0500

    редактирование первой таблицы

---
+++ edit.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<html>
+<head>	
+<title>Хабибуллина Д.</title> 
+</head>
+<body style="color: #636bc7">
+<h4>Хабибуллина Д.</h4>
+<h1>ЛР №2</h1>
+<h2>Вариант 11</h2>
+<h2>Редактировать язык программирования</h2>
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "progr" ); 
+mysqli_query($connect, "SET NAMES utf8");
+$rows=mysqli_query($connect, "SELECT name, type, year, vipl, author  FROM progrlanguages WHERE id=".$_GET['id']);
+	while ($st = mysqli_fetch_array($rows)) {
+	$id=$_GET['id'];
+	$name  = $st['name'];
+	$type = $st['type'];
+	$year  = $st['year'];
+	$vipl = $st['vipl'];
+	$author = $st['author'];
+	}
+print "<form action='editaction.php' metod='get'>";
+print "Марка: <input name='name' size='20' type='varchar'value='".$name."'>";
+print "<br>Модель: <input name='type' size='20' type='varchar'value='".$type."'>";
+print "<br>Тип разморозки: <input name='year' size='20' type='varchar'value='".$year."'>";
+print "<br>Внутренний объем: <input name='vipl' size='10' type='varchar'value='".$vipl."'>";
+print "<br>Срок гарантии: <input name='author' size='20' type='varchar'value='".$author."'>";
+print "<input type='hidden' name='id' value='".$id."'> <br>";
+print "<input type='submit' name='' value='Отредактировать'>";
+print "</form>";
+print "<p><a href=\"laba4.php\"> Вернуться к списку языков</a>";
+?>  
+</div>
+</body>
+</html>
---
+++ editaction.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+<head>	
+<title>Хабибуллина Д.</title> 
+</head>
+<body style="color: #636bc7">
+<h4>Хабибуллина Д.</h4>
+<h1>ЛР №2</h1>
+<h2>Вариант 11</h2>
+<h2>Редактировать язык программирования</h2>
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "progr" ); 
+mysqli_query($connect, "SET NAMES utf8");
+$zapros="UPDATE progrlanguages SET name='".$_GET['name']."', type='".$_GET['type']."', year='".$_GET['year']."', vipl='".$_GET['vipl']."', author='".$_GET['author']."'WHERE id=".$_GET['id'];
+mysqli_query($connect, $zapros);
+if (mysqli_affected_rows($connect)>0) {
+echo 'Все сохранено. <a href="laba4.php"> <br>Вернуться к списку языков</a>'; }
+else { echo 'Ошибка сохранения. <a href="laba4.php"><br> Вернуться к списку языков</a> '; }
+?>	
+</div>
+</body>
+</html>

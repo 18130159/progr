96ac052669b3ea3d57171f0afbf1ba637f872345
18130159 <59139041+18130159@users.noreply.github.com>
2020-12-03 18:23:26 +0500

    удаление записей из таблицы 3

---
+++ deleteapp.php
@@ -0,0 +1,8 @@
+﻿<?php
+$connect = mysqli_connect("localhost", "root", "root", "progr" );
+mysqli_query($connect, "SET NAMES utf8");
+$zapros="DELETE FROM app WHERE id=".$_GET['id'];
+mysqli_query($connect, $zapros);
+echo "<script>window.location.href='laba4.php'</script>";
+exit;
+?>

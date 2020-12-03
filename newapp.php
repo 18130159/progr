98c5363530b43e4894e10a04c157edb31521e40b
18130159 <59139041+18130159@users.noreply.github.com>
2020-12-03 18:24:51 +0500

    новое приложение

---
+++ newapp.php
@@ -0,0 +1,66 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		
+		<title>Хабибуллина Д.</title> 
+	</head>
+	<body style="color: #636bc7">
+<center>
+		<h4>Хабибуллина Д.</h4>
+		<h1>ЛР №5</h1>
+		<h2>Вариант 11</h2>
+<h2>Добавить нового разработчика</h2>
+<?php 
+		$connect = mysqli_connect("localhost", "root", "root", "progr");
+		mysqli_query($connect, "SET NAMES utf8");
+
+$rows1 = mysqli_query($connect, "SELECT id, nominal FROM developer ORDER BY id");
+$i=0;
+        while ($st = mysqli_fetch_array($rows1)) {
+            $nominal[$i] = $st['nominal'];
+            $id_zh[$i] = $st['id'];
+            $i++;
+        }
+$rows2 = mysqli_query($connect, "SELECT id, name FROM progrlanguages ORDER BY id");
+$j=0;
+        while ($st1 = mysqli_fetch_array($rows2)) {
+            $name[$j] = $st1['name'];
+            $id_zh1[$j] = $st1['id'];
+            $j++;
+        }
+?>
+<form action="newappaction.php" metod="get">
+Разработчик: <select name='nominal'>
+        <?php
+        for($i = 0; $nominal[$i] != null; $i++): ?>
+            <option value="<?=$nominal[$i].$i?>"><?=$nominal[$i]?></option>
+        <?php endfor;
+        for($i = 0; $nominal[$i] != null; $i++){
+$nominal = "num_res". $i;
+$value = "" . $id_zh[$i];
+print "<input type='hidden' name='".$nominal."' value='".$value."'>";
+}
+       ?>
+<br>Язык программирования:
+<select name='name'>
+        <?php
+        for($j = 0; $name[$j] != null; $j++): ?>
+            <option value="<?=$name[$j].$j?>"><?=$name[$j]?></option>
+        <?php endfor;
+        for($j = 0; $name[$j] != null; $j++){
+$name = "num_resn". $j;
+$value1 = "" . $id_zh1[$j];
+print "<input type='hidden' name='".$name."' value='".$value1."'>";
+}
+        ?>
+<br>Дата создания: <input name="date" size="30" type="date">
+<br>Текущая версия: <input name="version" size="30" type="varchar">
+<br>Название: <input name="title" size="30" type="varchar">
+</textarea>
+<p><input name="add" type="submit" value="Добавить">
+</form>
+<p>  
+</div>
+</center>
+	</body>
+</html>
---
+++ newappaction.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		
+		<title>Хабибуллина Д.</title> 
+	</head>
+	<body style="color: #636bc7">
+<center>
+		<h4>Хабибуллина Д.</h4>
+		<h1>ЛР №5</h1>
+		<h2>Вариант 11</h2>
+<h2>Добавить новое приложение</h2>
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "progr" ); 
+mysqli_query($connect, "SET NAMES utf8");
+$get_fio = $_GET['name'];
+	$name = substr($_GET['name'], 0, (strlen($_GET['name'])-1));
+	$num = substr($_GET['name'], -1, 1);
+	$v = "num_resn". $num;
+	$hid = "" . $_GET[$v];
+$str = (int)$hid;
+
+$get_fio1 = $_GET['nominal'];
+	$name1 = substr($_GET['nominal'], 0, (strlen($_GET['nominal'])-1));
+	$num1 = substr($_GET['nominal'], -1, 1);
+	$v1 = "num_res". $num1;
+	$hid1 = "" . $_GET[$v1];
+$str1 = (int)$hid1;
+
+$sql_add="INSERT INTO app SET id_yaz='" .$str."', name_yaz='" .$name."', id_d='" .
+$str1."', nom_d='" .$name1."', date='".$_GET['date']."', version='".$_GET['version']."', title='"
+.$_GET['title']."'"; 
+
+
+mysqli_query($connect, $sql_add); 
+if (mysqli_affected_rows($connect)>0)
+{ print "<p>Спасибо, вы добавили приложение.";
+print "<p><a href=\"laba4.php\"> Вернуться к списку таблиц</a>"; }
+else { print "Ошибка сохранения.".mysqli_error($connect)." <a href=\"laba4.php\"> Вернуться к списку таблиц</a>"; }
+?>
+</div>
+</center>
+	</body>
+</html>

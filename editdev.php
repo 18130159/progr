da83ed688952b0257742102af1dca861b75b3e31
18130159 <59139041+18130159@users.noreply.github.com>
2020-12-03 18:21:47 +0500

    редактировани второй таблицы

---
+++ editdev.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+<head>	
+<title>Хабибуллина Д.</title> 
+</head>
+<body style="color: #636bc7">
+<center>
+<h4>Хабибуллина Д.</h4>
+<h1>ЛР №5</h1>
+<h2>Вариант 11</h2>
+<h2>Редактировать разработчика</h2>
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "progr" ); 
+mysqli_query($connect, "SET NAMES utf8");
+$rows=mysqli_query($connect, "SELECT nominal, city  FROM developer WHERE id=".$_GET['id']);
+	while ($st = mysqli_fetch_array($rows)) {
+	$id=$_GET['id'];
+	$nominal  = $st['nominal'];
+$city  = $st['city'];
+	}
+print "<form action='editdevaction.php' metod='get'>";
+print "Название: <input name='nominal' size='20' type='varchar'value='".$nominal."'>";
+print "<br>Город: <input name='city' size='20' type='varchar'value='".$city."'>";
+print "<input type='hidden' name='id' value='".$id."'> <br>";
+print "<input type='submit' name='' value='Отредактировать'>";
+print "</form>";
+print "<p><a href=\"laba4.php\"> Вернуться к списку языков</a>";
+?> 
+</center> 
+</div>
+</body>
+</html>
---
+++ editdevaction.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html>
+<head>	
+<title>Хабибуллина Д.</title> 
+</head>
+<body style="color: #636bc7">
+<center>
+<h4>Хабибуллина Д.</h4>
+<h1>ЛР №5</h1>
+<h2>Вариант 11</h2>
+<h2>Редактировать разработчика</h2>
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "progr" ); 
+mysqli_query($connect, "SET NAMES utf8");
+$zapros="UPDATE developer SET nominal='".$_GET['nominal']."', city='".$_GET['city']."'WHERE id=".$_GET['id'];
+mysqli_query($connect, $zapros);
+if (mysqli_affected_rows($connect)>0) {
+echo 'Все сохранено. <a href="laba4.php"> <br>Вернуться к списку </a>'; }
+else { echo 'Ошибка сохранения. <a href="laba4.php"><br> Вернуться к списку </a> '; }
+?>	
+</div></center>
+</body>
+</html>
